7dd37ac74257847de9316fe4aea62d593752637b
Mike Moore <michaelhmoore1@gmail.com>
2022-01-11 10:41:18 -0700

    basic move ui and logic

--- AITicTacToe/AITicTacToe/ContentView.swift
+++ AITicTacToe/AITicTacToe/ContentView.swift
@@ -11,6 +11,9 @@ struct ContentView: View {
     let columns: [GridItem] = [GridItem(.flexible()),
                                GridItem(.flexible()),
                                GridItem(.flexible())]
+    
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumanTurn = true
         
     var body: some View {
         GeometryReader { geometry in
@@ -24,10 +27,17 @@ struct ContentView: View {
                                 .frame(width: geometry.size.width/3 - 15,
                                        height: geometry.size.width/3 - 15)
                             
-                            Image(systemName: "xmark")
+                            Image(systemName: moves[i]?.indicator ?? "")
                                 .resizable()
                                 .frame(width: 40, height: 40)
                                 .foregroundColor(.white)
+                        }
+                        .onTapGesture {
+                            guard !spaceIsOccupied(at: i, in: moves) else { return }
+                            
+                            let player: Player = isHumanTurn ? .human : .computer
+                            moves[i] = Move(player: player, boardIndex: i)
+                            isHumanTurn.toggle()
                         }
                     }
                 }
@@ -35,6 +45,22 @@ struct ContentView: View {
             }
             .padding()
         }
+    }
+    
+    func spaceIsOccupied(at index: Int, in moves: [Move?]) -> Bool {
+        return moves.contains(where: { $0?.boardIndex == index })
+    }
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    var indicator: String {
+        return player == .human ? "xmark" : "circle"
     }
 }
 
